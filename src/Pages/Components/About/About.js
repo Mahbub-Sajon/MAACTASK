9417ed5a3cc7794916fccc58bc33d7316d72eaa4
Mahbub-Sajon <sajon.mahbub@gmail.com>
2022-07-23 02:25:19 +0600

    about us created

--- src/Pages/Components/About/About.js
+++ src/Pages/Components/About/About.js
@@ -3,10 +3,14 @@ import img1 from '../../../img/Pre order vs delivery 1.png'
 import img2 from '../../../img/Top Selling Routes 1.png';
 import img3 from '../../../img/Avg 1.png'
 import person from '../../../img/Ellipse 1.png'
+import icon from '../../../img/monitor-mobbile.png'
+import { FaUsers } from "react-icons/fa";
+import {  AiOutlineUser, AiOutlineTrophy } from "react-icons/ai";
 
 const About = () => {
     return (
-        <div className='flex gap-36 pt-32'>
+      <div>
+          <div className='flex gap-36 pt-32'>
            <div> <div className=' ml-32 bg-blue-50 rounded-lg w-80 h-80'>
             </div>
             <div>
@@ -27,9 +31,31 @@ const About = () => {
                     <br />
                     <span className='font-semibold'>CEO, FieldX</span></p>
                 </div>
-                <p></p>
             </div>
         </div>
+        <div className='font-manrope flex justify-between mt-28 mx-36'>
+            <div>
+            <img className='mx-auto w-8 h-8' src={icon} alt="" />
+            <p className='text-center font-bold text-blue-700 text-3xl my-4'>10 +</p>
+            <p className='text-sm mx-auto'>Platforms Created</p>
+            </div>
+            <div className='text-center'>
+            <FaUsers className=' text-3xl mx-auto text-blue-700'></FaUsers>
+            <p className='font-bold text-blue-700 text-3xl my-4'>1559+</p>
+            <p className='text-sm'>Total Users</p>
+            </div>
+            <div className='text-center'>
+           <AiOutlineUser className='text-3xl mx-auto text-blue-700'></AiOutlineUser>
+           <p className='font-bold text-blue-700 text-3xl my-4'>10+</p>
+           <p className='text-sm'>Total Clients</p>
+            </div>
+            <div className='text-center'>
+            <AiOutlineTrophy className='text-3xl mx-auto text-blue-700'></AiOutlineTrophy>
+            <p className='font-bold text-blue-700 text-3xl my-4'>300 Days</p>
+            <p className='text-sm'>In Operations</p>
+            </div>
+        </div>
+      </div>
     );
 };
 
